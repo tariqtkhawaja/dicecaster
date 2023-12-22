453f1945073a6fd809ffbc903d4042d8410f52f1
Tariq Khawaja <tariqkhawaja26@live.com>
2023-12-22 23:17:47 +0000

    write tests for useDice

--- src/hooks/useDice/useDice.ts
+++ src/hooks/useDice/useDice.ts
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
-export const rollDie = (numberOfSides: number) => Math.floor(Math.random() * numberOfSides) + 1
+export const rollDie = (numberOfSides: number): number => Math.floor(Math.random() * numberOfSides) + 1
 
 export const useDice = (numberOfSides: number) => {
     const [value, setValue] = useState<number>(0);
     const [history, setHistory] = useState<number[]>([]);
-    
+
     const roll = () => {
-      let rolledResult = rollDie(numberOfSides);
-      setValue(rolledResult);
-      setHistory(prev => [...prev, rolledResult]);
+        let rolledResult = rollDie(numberOfSides);
+        setValue(rolledResult);
+        setHistory(prev => [...prev, rolledResult]);
     };
 
     return { history, roll, value };
